7e16387e02c807d45e28eabb314ffb0c1ad4d4ed
Kristel Salazar <69431960+kris01-s@users.noreply.github.com>
2025-06-25 21:26:45 -0600

    chore: add themes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import type { User } from "@supabase/supabase-js";
 import { supabase } from "./supabase.js";
 import { AppRoutes } from "./routes/AppRoutes.js";
+import { ThemeToggle } from "./components/custom/ThemeToggle.js";
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
@@ -15,7 +16,12 @@ function App() {
     });
   }, []);
 
-  return <AppRoutes />;
+  return (
+    <div className="  w-screen h-screen flex flex-col items-start justify-start">
+      <ThemeToggle />
+      <AppRoutes />
+    </div>
+  );
 }
 
 export default App;
